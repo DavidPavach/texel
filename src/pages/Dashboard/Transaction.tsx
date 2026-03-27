336c8732fa8872fd5c9dcc6dc67d8b9e8ab5097f
DavidPavach <davidpavach@proton.me>
2026-03-27 22:32:10 +0100

    feat: quick money

--- src/pages/Dashboard/Transaction.tsx
+++ src/pages/Dashboard/Transaction.tsx
@@ -2,13 +2,15 @@
 import TransactionActivityCard from "./TransactionCard";
 
 //Components
-import { GetUserLastThreeTransactions } from "@/services/queries.service";
+import { GetAllTransactions } from "@/services/queries.service";
 import ErrorDisplay from "@/components/Error";
+
+// GetUserLastThreeTransactions
 
 
 const Transaction = () => {
 
-    const { data, isLoading, isError, isFetching, refetch } = GetUserLastThreeTransactions();
+    const { data, isLoading, isError, isFetching, refetch } = GetAllTransactions("1", "8");
 
     return (
         <main>
@@ -26,7 +28,7 @@ const Transaction = () => {
                     ))}
                 </div>
             ) : (
-                <TransactionActivityCard title="Recent Activity" transactions={data.data} />
+                <TransactionActivityCard title="Recent Activity" transactions={data.data.data} />
             )}
         </main>
     );
